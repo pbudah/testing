162ce97b14377becb0d3e5936696bd4b70826725
Konstantin Stepanov <kostja.stepanov@gmail.com>
2016-10-31 16:11:05 +0200

    Added bubble_sort algorith in sort function in sample_app.py

--- sample_app/sample_app.py
+++ sample_app/sample_app.py
@@ -1,3 +1,11 @@
 # should sort the list using any algorithm
-def sort():
-    pass  # implement
+def sort(lst):
+    n = 1
+    while n < len(lst):
+        for i in range(len(lst)-n):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+        n += 1
+
+    return lst
+
