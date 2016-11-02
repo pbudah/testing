12d7561e610919b661791f91cd4b8e5f5c2d87d7
Konstantin Stepanov <kostja.stepanov@gmail.com>
2016-11-02 22:01:07 +0200

    Adde two tests in intersect_test.py

--- unit_tests/intersect_test.py
+++ unit_tests/intersect_test.py
@@ -10,3 +10,18 @@ class IntersectTest(unittest.TestCase):
 
     def test_inter1(self):
         self.assertEqual(self.result, intersect(self.a, self.b))
+
+    def test_inter2(self):
+        self.assertNotIn(self.result, self.a)
+        self.assertNotIn(self.result, self.b)
+
+    def test_inter3(self):
+        self.assertEqual([], intersect(self.a, []))
+        self.assertEqual([], intersect([],[]))
+        self.assertEqual([], intersect([],self.b))
+
+    def tearDown(self):
+        print 'tearDown is executed after each test method'
+
+if __name__ == '__main__':
+    unittest.main()
