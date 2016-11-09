46d47cb677fd0cdde17438e6d24d9b90a64ece39
Konstantin Stepanov <kostja.stepanov@gmail.com>
2016-11-09 14:19:28 +0200

    Added text messages in assertions in intersect_test.py

--- unit_tests/intersect_test.py
+++ unit_tests/intersect_test.py
@@ -11,19 +11,19 @@ class IntersectTest(unittest.TestCase):
 
     def test_inter1(self):
         res = intersect(self.a,self.b)
-        self.assertEqual(self.result, res)
+        self.assertEqual(self.result, res,'unexpected result')
 
     def test_inter2(self):
-        self.assertNotIn(self.result, self.a)
-        self.assertNotIn(self.result, self.b)
+        self.assertNotIn(self.result, self.a, 'set is in result set')
+        self.assertNotIn(self.result, self.b, 'set is in result set')
 
     def test_inter3(self):
         res1 = intersect(self.a, [])
         res2 = intersect([],[])
         res3 = intersect([],self.b)
-        self.assertEqual([], res1)
-        self.assertEqual([], res2)
-        self.assertEqual([], res3)
+        self.assertEqual([], res1, 'incorrect intersection with an empty list')
+        self.assertEqual([], res2, 'incorrect intersection of two empty lists')
+        self.assertEqual([], res3, 'incorrect intersection with an empty list')
 
     def tearDown(self):
         print 'tearDown is executed after each test method'
