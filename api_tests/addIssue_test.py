9f722bd41a8eafb25a4e37a790c560b5d4b710d5
Konstantin Stepanov <kostja.stepanov@gmail.com>
2016-11-13 21:57:35 +0200

    Added delete request in tearDown method

--- api_tests/addIssue_test.py
+++ api_tests/addIssue_test.py
@@ -9,14 +9,17 @@ class AddIssueTest(unittest.TestCase):
         payload = "{\r\n            \"fields\": {\r\n                \"project\": {\r\n                    \"key\": \"MED\"\r\n                },\r\n                \"summary\": \"Not bad.\",\r\n                \"description\": \"Creating new issue\",\r\n                \"issuetype\": {\r\n                    \"name\": \"Bug\"\r\n                }\r\n            }\r\n        }\r\n        "
         auth = ('restadmin', 'restadmin')
         self.prst = requests.post('http://13.92.28.188:8090/rest/api/2/issue',data=payload, auth=auth, headers=headers)
-
+        p_json = self.prst.json()
+        self.k = p_json['key']
 
     def test_create(self):
         r = self.prst.status_code
         self.assertEqual(r, ADDED, 'request is not valid and the issue could not be created')
 
-    #def tearDown(self):
-        #requests.delete('http://13.92.28.188:8090/rest/api/2/issue/MED-35', auth=('restadmin','restadmin'))
+        #print self.p_json['key']
+
+    def tearDown(self):
+        requests.delete('http://13.92.28.188:8090/rest/api/2/issue/'+ self.k, auth=('restadmin','restadmin'))
 
 
 if __name__ == '__main__':
